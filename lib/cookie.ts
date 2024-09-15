99c4146d8a036917f8c22d63b14e82b1e247bd43
Genichi Maruo <gen1010f8e7@gmail.com>
2024-09-16 04:37:45 +0900

    Add cookie utility functions for setting and getting cookies

---
+++ lib/cookie.ts
@@ -0,0 +1,17 @@
+export const setCookie = (name: string, value: string, days: number) => {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value};${expires};path=/;secure`;
+};
+
+export const getCookie = (name: string) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift();
+  return null;
+};
+
+export const deleteCookie = (name: string) => {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+};
