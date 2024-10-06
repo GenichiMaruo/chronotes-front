6f04f980c9ff89895f38b4364df9c05573832abc
Genichi Maruo <gen1010f8e7@gmail.com>
2024-10-06 17:04:59 +0900

    Refactor API provider and cookie handling

--- lib/cookie.ts
+++ lib/cookie.ts
@@ -6,6 +6,7 @@ export const setCookie = (name: string, value: string, days: number) => {
 };
 
 export const getCookie = (name: string) => {
+  if (typeof window === 'undefined') return null;
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()?.split(';').shift();
